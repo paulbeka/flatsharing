35bae7d854f8d66ad5281a93ba64d8fcef968450
Paul Bekaert <2517073B@student.gla.ac.uk>
2023-10-19 17:55:40 +0100

    initialized basic app

--- app/index.js
+++ app/index.js
@@ -1,14 +1,57 @@
 import {
+  StyleSheet,
   View,
-  Text 
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Dimensions,
+  Button
 } from 'react-native'
+import { Stack, useRouter } from 'expo-router'
+
+import { SIZES } from '../constants/themes.js'
 
 const App = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
+    <SafeAreaView  style={styles.app}>
+      <Stack.Screen 
+        options={{
+          title: 'Flatsharing',
+          headerStyle: styles.header
+        }}
+        style={styles.navbar}
+      />
+      <ScrollView showVerticalScrollIndicator={false}>
+        <View style={{
+          flex: 1,
+          padding: SIZES.medium 
+        }}>
+          <Text>Welcome to the flatmates sharing app!</Text>
+          <Button title={"Get Started!"} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  app: {
+    backgroundColor: '#bdf8ff',
+    flex: 1,
+    paddingTop: 40,
+  },
+  header: {
+    position: 'absolute',
+    top: Dimensions.get('window').height - 60,
+    left: 0,
+    right: 0,
+    height: 60,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default App
