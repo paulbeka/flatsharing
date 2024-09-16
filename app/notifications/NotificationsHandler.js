f1a67753d1e0787d5c13b36d9019d1b9bae04846
Paul Bekaert <2517073B@student.gla.ac.uk>
2024-09-16 19:59:23 +0100

    refactored app structure

--- app/notifications/NotificationsHandler.js
+++ app/notifications/NotificationsHandler.js
@@ -2,8 +2,6 @@ import * as Notifications from "expo-notifications";
 
 
 
-
-
 // When loading the data from server, check what has already been scheduled
 // and when. If it has been scheduled, but now it's at a different time,
 // then cancel the schedule and re-schedule.
